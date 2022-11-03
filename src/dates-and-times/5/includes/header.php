97d2f04da524cd72d7b2543e70ff5ff8308b91a2
Patryk Pilek <patryk.pilek@gmail.com>
2022-11-03 23:59:13 +0000

    Publish unpublished articles using an Ajax request

---
+++ src/dates-and-times/5/includes/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My blog</title>
+    <meta charset="utf-8">
+</head>
+<body>
+
+    <header>
+        <h1>My blog</h1>
+    </header>
+
+    <nav>
+        <ul>
+            <li><a href="/dates-and-times/5/">Home</a></li>
+
+            <?php if (Auth::isLoggedIn()) : ?>
+                
+                <li><a href="/dates-and-times/5/admin/">Admin</a></li>
+                <li><a href="/dates-and-times/5/logout.php">Log out</a></li>
+
+            <?php else : ?>
+                
+                <li><a href="/dates-and-times/5/login.php">Log in</a></li>
+
+            <?php endif; ?>
+        </ul>
+    </nav>
+
+    <main>
